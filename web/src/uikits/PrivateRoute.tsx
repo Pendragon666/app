61a660b03a0fe0375743e47e443b5dd80c4bd3c2
harukaze-sm <sebastian.maisuradze.5@btu.edu.ge>
2021-08-19 02:03:51 +0400

    finalizing socket io

--- web/src/uikits/PrivateRoute.tsx
+++ web/src/uikits/PrivateRoute.tsx
@@ -1,3 +1,4 @@
+import { CircularProgress } from '@material-ui/core';
 import axios from 'axios';
 import Cookie from 'js-cookie';
 import decode from 'jwt-decode';
@@ -5,11 +6,11 @@ import { CreateProfile } from 'layouts';
 import { useEffect, useMemo } from 'react';
 import { useDispatch } from 'react-redux';
 import { Route, Redirect } from 'react-router-dom';
-// import { setInvite } from 'redux/actions/uiActions';
+import { setInvite } from 'redux/actions/uiActions';
 import { getProfile, setUser } from 'redux/actions/userActions';
 import { useAppSelector } from 'redux/hooks';
 import { UserState } from 'redux/reducers/userReducer';
-// import io from 'socket.io-client';
+import io from 'socket.io-client';
 
 const CheckAuth = () => {
   let token = Cookie.get('P-Token');
@@ -33,16 +34,6 @@ const CheckAuth = () => {
   return { authenticated: true, user, token };
 };
 
-// const Loading = (WrappedComponent: any) => {
-//   // And return another component
-//   const LoadingComponent = ({ Component, ...rest}) => {
-//     const loading = useAppSelector((state) => state.UI.loading);
-
-//     return <> {loading ? <h1>Loading...</h1> : <Component {...rest} />}</>;
-//   };
-//   return LoadingComponent;
-// };
-
 const PrivateRoute = ({ component: Component, ...rest }: any) => {
   const state = useMemo(() => CheckAuth(), []);
 
@@ -51,23 +42,24 @@ const PrivateRoute = ({ component: Component, ...rest }: any) => {
 
   const dispatch = useDispatch();
   const SetUser = (data: any) => dispatch(setUser(data));
-  // const SetInvite = (data: any) => dispatch(setInvite(data));
+  const SetInvite = (data: any) => dispatch(setInvite(data));
   const GetProfile = () => dispatch(getProfile());
 
-  // useEffect(() => {
-  //   const socket = io('http://localhost:5000/', {
-  //     query: { token: state.token },
-  //     // path: '/api/socket.io',
-  //     // secure: process.env.NODE_ENV === 'production',
-  //   });
-  //   if (!Profile.profile?.inTeam && Profile.profile?.inTeam !== undefined) {
-  //     socket.once('team_invitation', (msg) => {
-  //       const { _id, teamName, teamImage, invitationId } = JSON.parse(msg);
-  //       SetInvite({ teamInvite: { id: _id, name: teamName, image: teamImage, invited: true, invitationId } });
-  //     });
-  //   }
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, [Profile]);
+  const uri = process.env.NODE_ENV === 'production' ? 'ws://development.pendragon.gg' : 'ws://localhost';
+
+  useEffect(() => {
+    const socket = io(uri, {
+      query: { token: state.token },
+      secure: process.env.NODE_ENV === 'production',
+    });
+    if (!Profile.profile?.inTeam && Profile.profile?.inTeam !== undefined) {
+      socket.once('team_invitation', (msg) => {
+        const { _id, teamName, teamImage, invitationId } = JSON.parse(msg);
+        SetInvite({ teamInvite: { id: _id, name: teamName, image: teamImage, invited: true, invitationId } });
+      });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [Profile]);
 
   useEffect(() => {
     if (state.authenticated) {
@@ -94,7 +86,11 @@ const PrivateRoute = ({ component: Component, ...rest }: any) => {
         }
       />
     );
-  return <p>Hold on, fetching data might take some time.</p>;
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100vw', height: '100vh' }}>
+      <CircularProgress color="secondary" />
+    </div>
+  );
 };
 
 export default PrivateRoute;
