c76f2f249f19a9d824a30c0dc8996c8ba9db4152
harukaze-sm <sebastian.maisuradze.5@btu.edu.ge>
2021-08-30 23:43:15 +0400

    code splitting

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, lazy, Suspense } from 'react';
+import React, { useEffect } from 'react';
 // import io, { Socket } from 'socket.io-client';
 import { SnackbarProvider } from 'notistack';
 
@@ -23,18 +23,15 @@ import {
   DialogContentText,
   DialogTitle,
   Button,
-  CircularProgress,
 } from '@material-ui/core';
 import { Alert } from '@material-ui/lab';
 import { clearMessage, respondToInvite } from 'redux/actions/uiActions';
 import { Workbox } from 'workbox-window';
 import { useAppDispatch, useAppSelector } from 'redux/hooks';
 import { UIState } from 'redux/reducers/uiReducer';
+import { PrivateRoute, PublicRoute } from 'uikits';
 
 const App: React.FC = () => {
-  const PrivateRouteComponent = lazy(() => import('./uikits/PrivateRoute'));
-  const PublicRouteComponent = lazy(() => import('./uikits/PublicRoute'));
-
   useEffect(() => {
     if ('serviceWorker' in navigator && process.env.NODE_ENV === 'production') {
       const wb = new Workbox('/sw.js');
@@ -69,70 +66,56 @@ const App: React.FC = () => {
 
   return (
     <>
-      <Suspense
-        fallback={() => (
-          <div
-            style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100vw', height: '100vh' }}
-          >
-            <CircularProgress color="secondary" />
-          </div>
-        )}
-      >
-        <SnackbarProvider maxSnack={3}>
-          <Router>
-            <Switch>
-              {/* Public Routes */}
-              <PublicRouteComponent path="/login" component={LoginPage} exact />
-              <PublicRouteComponent path="/register" component={RegisterPage} exact />
+      <SnackbarProvider maxSnack={3}>
+        <Router>
+          <Switch>
+            {/* Public Routes */}
+            <PublicRoute path="/login" component={LoginPage} exact />
+            <PublicRoute path="/register" component={RegisterPage} exact />
 
-              {/* Private Routes */}
-              <PrivateRouteComponent path="/" component={HomePage} exact />
-              <PrivateRouteComponent path="/leaderboard" component={LeaderboardPage} exact />
-              <PrivateRouteComponent
-                path="/profile/:uid"
-                component={(props: any) => <ProfilePage {...props} />}
-                exact
-              />
-              <PrivateRouteComponent path="/tournaments" component={TournamentPage} exact />
-              <PrivateRouteComponent path="/teams" component={TeamsPage} exact />
-              <PrivateRouteComponent
-                path="/tournaments/:id"
-                component={(props: any) => <SingleTournamentPage {...props} />}
-                exact
-              />
+            {/* Private Routes */}
+            <PrivateRoute path="/" component={HomePage} exact />
+            <PrivateRoute path="/leaderboard" component={LeaderboardPage} exact />
+            <PrivateRoute path="/profile/:uid" component={(props: any) => <ProfilePage {...props} />} exact />
+            <PrivateRoute path="/tournaments" component={TournamentPage} exact />
+            <PrivateRoute path="/teams" component={TeamsPage} exact />
+            <PrivateRoute
+              path="/tournaments/:id"
+              component={(props: any) => <SingleTournamentPage {...props} />}
+              exact
+            />
 
-              <Route path="/" render={() => <div>NOT FOUND</div>} />
-            </Switch>
-          </Router>
-          <Snackbar
-            open={UI.show}
-            autoHideDuration={2500}
-            onClose={() => ClearMessage()}
-            anchorOrigin={{ vertical: 'top', horizontal: 'left' }}
+            <Route path="/" render={() => <div>NOT FOUND</div>} />
+          </Switch>
+        </Router>
+        <Snackbar
+          open={UI.show}
+          autoHideDuration={2500}
+          onClose={() => ClearMessage()}
+          anchorOrigin={{ vertical: 'top', horizontal: 'left' }}
+        >
+          <Alert onClose={() => ClearMessage()} severity={UI.type}>
+            {UI.message}
+          </Alert>
+        </Snackbar>
+        <Dialog open={UI.teamInvite.invited} aria-labelledby="responsive-dialog-title">
+          <DialogTitle id="responsive-dialog-title">Invitation</DialogTitle>
+          <DialogContent
+            style={{ display: 'flex', alignItems: 'center', flexDirection: 'column', textAlign: 'center' }}
           >
-            <Alert onClose={() => ClearMessage()} severity={UI.type}>
-              {UI.message}
-            </Alert>
-          </Snackbar>
-          <Dialog open={UI.teamInvite.invited} aria-labelledby="responsive-dialog-title">
-            <DialogTitle id="responsive-dialog-title">Invitation</DialogTitle>
-            <DialogContent
-              style={{ display: 'flex', alignItems: 'center', flexDirection: 'column', textAlign: 'center' }}
-            >
-              <img src={UI.teamInvite.image} alt="team" style={{ height: 70, marginBottom: 20 }} />
-              <DialogContentText>You've been invited to team {UI.teamInvite.name}.</DialogContentText>
-            </DialogContent>
-            <DialogActions>
-              <Button onClick={() => handleClose(false)} color="secondary">
-                Decline
-              </Button>
-              <Button onClick={() => handleClose(true)} color="secondary">
-                Accept
-              </Button>
-            </DialogActions>
-          </Dialog>
-        </SnackbarProvider>
-      </Suspense>
+            <img src={UI.teamInvite.image} alt="team" style={{ height: 70, marginBottom: 20 }} />
+            <DialogContentText>You've been invited to team {UI.teamInvite.name}.</DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={() => handleClose(false)} color="secondary">
+              Decline
+            </Button>
+            <Button onClick={() => handleClose(true)} color="secondary">
+              Accept
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </SnackbarProvider>
     </>
   );
 };
